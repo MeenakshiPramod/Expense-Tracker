088cf9df59d8ecded0223bf637556438dde50f82
Meenakshi Pramod <meenakshipramod04@gmail.com>
2026-02-02 18:45:25 +0530

    DTO created

---
+++ src/main/java/com/expensetracker/user/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.expensetracker.user.service;
+
+import com.expensetracker.user.entity.User;
+import com.expensetracker.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+}
